aad459ccc1a6d86b2dc7832305fbf7b21e607178
WebDvlper <web@dvlper.com>
2016-07-07 19:31:42 +0530

    Setup S3 keys for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,11 @@ Rails.application.configure do
 
   # config/environments/production.rb
   config.paperclip_defaults = {
-:storage => :s3,
-:s3_credentials => {
-  :bucket => ENV['AWS_BUCKET'],
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-}
-}
+    storage: :s3,
+    s3_credentials: {
+      bucket: 'lbdr-production',
+      access_key_id: 'AKIAI244XHG7LQB4OGDA',
+      secret_access_key: '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' 
+    }
+  }
 end
